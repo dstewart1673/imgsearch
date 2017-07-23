47416e934c76b551cd349dee5ac067f0d55a9b59
DStewart1673 <DStewart1673@gmail.com>
2017-07-22 17:27:51 -0700

    added node to package

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongodb = require('mongodb');
 const fs = require('fs');
 const path = require('path');
-
 const port = process.env.PORT || 8080;
 const app = express();
 
